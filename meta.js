6fdd095a4b02caf63b18f68aac0169fa7783c672
Icebob <mereg.norbert@gmail.com>
2017-05-15 12:26:26 +0200

    add cacher & transporter

--- meta.js
+++ meta.js
@@ -8,6 +8,41 @@ module.exports = function(values) {
 				name: "apiGW",
 				message: "Add API Gateway (moleculer-web) service",
 				default: true
+			},
+			{
+				type: "confirm",
+				name: "needTransporter",
+				message: "Would you like communicate with other nodes?",
+				default: false
+			},
+			{
+				type: "list",
+				name: "transporter",
+				message: "Select a transporter",
+				choices: [
+					{ name: "NATS (recommended)", value: "nats" },
+					{ name: "MQTT", value: "mqtt" },
+					{ name: "Redis", value: "redis" }
+				],
+				when(answers) { return answers.needTransporter; },
+				default: "nats"
+			},
+			{
+				type: "confirm",
+				name: "needCacher",
+				message: "Would you like use cache?",
+				default: false
+			},
+			{
+				type: "list",
+				name: "cacher",
+				message: "Select a cacher solution",
+				choices: [
+					{ name: "Memory", value: "memory" },
+					{ name: "Redis", value: "redis" }
+				],
+				when(answers) { return answers.needCacher; },
+				default: "memory"
 			}
 		],
 
--- template/index.js
+++ template/index.js
@@ -1,12 +1,27 @@
 "use strict";
 
-let { ServiceBroker } = require("moleculer");
+let { ServiceBroker{{#needTransporter}}, Transporters{{/needTransporter}}{{#needCacher}}, Cachers{{/needCacher}} } = require("moleculer");
 {{#apiGW}}
 let ApiGateway = require("moleculer-web");
 {{/apiGW}}
 
 // Create broker
 let broker = new ServiceBroker({
+	{{#if_eq transporter "nats"}}
+	transporter: new Transporters.NATS(),
+	{{/if_eq}}
+	{{#if_eq transporter "mqtt"}}
+	transporter: new Transporters.MQTT(),
+	{{/if_eq}}
+	{{#if_eq transporter "redis"}}
+	transporter: new Transporters.Redis(),
+	{{/if_eq}}
+	{{#if_eq cacher "memory"}}
+	cacher: new Cachers.Memory(),
+	{{/if_eq}}
+	{{#if_eq cacher "redis"}}
+	cacher: new Cachers.Redis(),
+	{{/if_eq}}
 	logger: console,
 	logLevel: process.env.LOG_LEVEL || "info"
 });
